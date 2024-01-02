e89a907fcf456bff0e0050bdc19fb7d0bef7b39e
yannivador <yann901@hotmail.fr>
2024-01-02 12:12:54 +0400

    CV with test Mysql : lecture dans la base

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+
+
+## MySQL
+source : https://blog.arcoptimizer.com/utilisation-de-mysql-avec-node-js-et-le-client-javascript-mysql 
+
+
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +78,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
 
 const portfolioData = [
-  {
-      id: 1,
-      name: 'IRE',
-      languages: ['CSS', 'HTML'],
-      source: "dfgfdgfd",
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
-      picture: './IRE.png'
-  },
-  {
-      id: 2,
-      name: 'Rando',
-      languages: ['CSS', 'Javascript', 'PHP', 'HTML', 'MYSQL'],
-      source: "dfgfdgfd",
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
-      picture: './img-rando.png'
-  },
-  {
-      id: 3,
-      name: 'd-angle',
-      languages: ['CSS', 'HTML'],
-      source: "dfgfdgfd",
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
-      picture: './d-angle.png'
-  },
-  {
-      id: 4,
-      name: 'Booking',
-      languages: ['CSS', 'Javascript', 'PHP', 'HTML', 'Mysql'],
-      source: "dfgfdgfd",
-      info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
-      picture: './img-booking.png'
-  }
+    {
+        id: 1,
+        name: 'IRE',
+        languages: ['CSS', 'HTML'],
+        source: "dfgfdgfd",
+        info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
+        picture: './IRE.png'
+    },
+    {
+        id: 2,
+        name: 'Rando',
+        languages: ['CSS', 'Javascript', 'PHP', 'HTML', 'MYSQL'],
+        source: "dfgfdgfd",
+        info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
+        picture: './img-rando.png'
+    },
+    {
+        id: 3,
+        name: 'd-angle',
+        languages: ['CSS', 'HTML'],
+        source: "dfgfdgfd",
+        info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
+        picture: './d-angle.png'
+    },
+    {
+        id: 4,
+        name: 'Booking',
+        languages: ['CSS', 'Javascript', 'PHP', 'HTML', 'Mysql'],
+        source: "dfgfdgfd",
+        info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, animi quidem perferendis et at amet nulla dolorum natus odio ab qui pariatur eum iure cupiditate. Quis, magnam laudantium, animi aliquid esse vitae pariatur temporibus repellendus eligendi unde accusantium!z',
+        picture: './img-booking.png'
+    }
 ]
 
 const userData = [
@@ -64,6 +64,30 @@ const userData = [
     }
 ]
 
+const mysql = require('mysql');
+
+const db = mysql.createConnection({ 
+    host: "mysql-yannivador.alwaysdata.net", 
+    user: "238563", 
+    password: "ibanez.sa90" ,
+    database: "yannivador_coder-son-cv"
+});
+
+db.connect(function(err) {   
+    if (err) throw err;   
+    console.log("Connecté à la base de données MySQL!"); 
+});
+
+// const sqlQuery = db.query('SELECT * FROM projects');
+
+db.query ('SELECT * FROM users ', (err, lignes) => {
+    if (err) throw err;
+  
+    console.log ('Données reçues de Db:');
+    console.log (lignes);
+})
+
+
 const express = require('express');
 
 const app = express();
@@ -75,15 +99,15 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
     );
     res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, OPTIONS"
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, OPTIONS"
     );
     next();
-  });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello man !');
@@ -100,3 +124,4 @@ app.get('/users', (req, res) => {
 app.listen(port, () => {
     console.log(`Server at running on port ${port}`)
 })
+
